592447e0fb2eb01e759a479aeffb485730531c8a
Jessica Mallian <jlmallian@gmail.com>
2018-07-26 19:46:20 -0500

    created .swift files for the 3 ViewControllers

---
+++ MemeEditorV2/MemeCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeEditorV2
+//
+//  Created by Jessica Mallian on 7/26/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewController: UICollectionViewController {
+    
+}
---
+++ MemeEditorV2/MemeTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemeTableViewController.swift
+//  MemeEditorV2
+//
+//  Created by Jessica Mallian on 7/26/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewController: UITableViewController {
+    
+}
