ce021231e73f07d39ed53f283fc906a9db1da896
Jessica Mallian <jlmallian@gmail.com>
2018-07-26 19:49:12 -0500

    created Meme struct

---
+++ MemeEditorV2/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeEditorV2
+//
+//  Created by Jessica Mallian on 7/26/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    var topText: String!
+    var bottomText: String!
+    var pic: UIImage!
+    var memedImage: UIImage
+}
