dc3dbcedbfd7afd8b40f54fbfe96ee5e34cc0136
Jessica Mallian <jlmallian@gmail.com>
2018-07-27 14:21:19 -0500

    created MemeDetailViewController and MemeEditorViewController

---
+++ MemeEditorV2/MemeDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemeDetailViewController.swift
+//  MemeEditorV2
+//
+//  Created by Jessica Mallian on 7/27/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController {
+    
+}
