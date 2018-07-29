9f048cc21ed46b9473dd258a9a7f6d899c33530f
Jessica Mallian <jlmallian@gmail.com>
2018-07-29 13:47:06 -0500

    added function setTextFieldAttributesAndDelegate from V! and changed the UILabels to UITextfields in MemeEditor

--- MemeEditorV2/MemeEditorViewController.swift
+++ MemeEditorV2/MemeEditorViewController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
-class MemeEditorViewController: UIViewController {
+class MemeEditorViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: properties
     @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var topLabel: UILabel!
-    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    //MARK: attritubtes for text fields
+    let memeTextAttributes: [String: Any] = [
+        NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+        NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+        NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedStringKey.strokeWidth.rawValue: -3
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //custimize text fields and set text field delegates
+        setTextFieldAttributesAndDelegate(text: "BOTTOM", textField: bottomTextField)
+        setTextFieldAttributesAndDelegate(text: "TOP", textField: topTextField)
+    }
+    
+    func setTextFieldAttributesAndDelegate(text: String, textField: UITextField) {
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.text = text
+        textField.backgroundColor = .clear
+        textField.autocapitalizationType = .allCharacters
+        textField.textAlignment = .center
+        textField.borderStyle = .none
+        textField.delegate = self
     }
 
 
